197bc7dab339644f747dada77f00b360e7838d05
yunze1012 <yunzhou1012@gmail.com>
2021-03-28 13:34:42 -0400

    deleted imgui

--- Pathfinding Visualizer/main.cpp
+++ Pathfinding Visualizer/main.cpp
@@ -3,6 +3,8 @@
 #include "Display.h"
 
 #include <SFML/Graphics.hpp>
+// #include <TGUI/TGUI.hpp>
+
 #include <iostream>
 #include <memory>
 #include <windows.h>
@@ -18,7 +20,9 @@ int main(int argc, char **argv) {
     d.run(g->getSquare(0, 0), g->getSquare(1, 2));
     d.print(g->getSquare(0, 0), g->getSquare(1, 2));
 
-    /*sf::RenderWindow window(sf::VideoMode(1280, 720), "Pathfinder Visualizer V0.0");
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -30,35 +34,9 @@ int main(int argc, char **argv) {
         }
 
         window.clear();
+        window.draw(shape);
         window.display();
-    }*/
-
-    /*priority_queue <shared_ptr<Square>, vector<shared_ptr<Square>>, greater<shared_ptr<Square>>> unvisitedNodes;
-    shared_ptr<Square> two = make_shared<Square>(0, 0);
-    two->setDistance(2);
-    shared_ptr<Square> one = make_shared<Square>(0, 1);
-    one->setDistance(1);
-    shared_ptr<Square> four = make_shared<Square>(0, 2);
-    four->setDistance(4);
-    shared_ptr<Square> three = make_shared<Square>(0, 3);
-    three->setDistance(3);
-    unvisitedNodes.emplace(one);
-    unvisitedNodes.emplace(two);
-    unvisitedNodes.emplace(three);
-    unvisitedNodes.emplace(four);
-    
-    cout << unvisitedNodes.top()->getDistance() << endl;
-    four->setDistance(0);
-    std::make_heap(const_cast<shared_ptr<Square>*>(&unvisitedNodes.top()),
-        const_cast<shared_ptr<Square>*>(&unvisitedNodes.top()) + unvisitedNodes.size(),
-        greater<shared_ptr<Square>>());
-
-    cout << unvisitedNodes.top()->getDistance() << endl;*/
-
-    /*while(unvisitedNodes.empty() == false) {
-        cout << unvisitedNodes.top()->getDistance() << endl;
-        unvisitedNodes.pop();
-    }*/
+    }
 
     return 0;
 }
